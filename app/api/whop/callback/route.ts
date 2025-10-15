83c360aed82d0ea1e17a6497ac6fd6b175ed6d48
youngmike7227-cloud <youngmike7227@gmail.com>
2025-10-14 21:26:31 -0400

    Update route.ts

--- app/api/whop/callback/route.ts
+++ app/api/whop/callback/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { exchangeCodeForToken, getMembershipStatus } from "@/lib/whop";
+import { exchangeCodeForToken, getMembershipStatus } from "../../../lib/whop";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
