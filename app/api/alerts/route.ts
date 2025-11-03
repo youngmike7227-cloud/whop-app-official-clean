7a6805a678b296bafc862e0d79f2d6c6d7264138
youngmike7227-cloud <youngmike7227@gmail.com>
2025-11-03 15:50:25 -0500

    Update route.ts

--- app/api/alerts/route.ts
+++ app/api/alerts/route.ts
@@ -1,6 +1,6 @@
 // app/api/alerts/recent/route.ts
 import { NextResponse } from "next/server";
-import { sql } from "../../../../lib/db"; // 👈 4 dots up
+import { sql } from "../../../lib/db"; // 👈 4 dots up
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
